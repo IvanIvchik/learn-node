22da50d81d9aca06e3cfa39da0098c311ac292c0
IvanIvchik <ivanivcik11@gmaol.com>
2022-07-21 22:19:37 +0300

    решение 2.10.1

--- tascs.js
+++ tascs.js
@@ -2,9 +2,9 @@ const { stdin, stdout } = process;
 
 stdout.write('Официальное название JavaScript?\n')
 stdin.on('data', data => {
-    const answer = data.toString();
+    const answer = data.toString().trim();
     if (answer === 'ECMAScript') {
         stdout.write('Всё верно!');
-    } else stdout.write('Не знаете? ECMAScript!');
+    } else { stdout.write('Не знаете? ECMAScript!'); }
     process.exit();
 });
