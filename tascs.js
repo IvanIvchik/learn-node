9f5e2ed4c1d3688c462f9637a2bb979adce7e56d
IvanIvchik <ivanivcik11@gmaol.com>
2022-07-21 22:31:08 +0300

    задание 2.10.2

--- tascs.js
+++ tascs.js
@@ -1,10 +1,14 @@
 const { stdin, stdout } = process;
 
-stdout.write('Официальное название JavaScript?\n')
+stdout.write('Введите число\n')
 stdin.on('data', data => {
-    const answer = data.toString().trim();
-    if (answer === 'ECMAScript') {
-        stdout.write('Всё верно!');
-    } else { stdout.write('Не знаете? ECMAScript!'); }
+    const answer = +data.toString().trim();
+    if (isNaN(answer)) {
+        stdout.write('Введите число!');
+    } else if(answer < 0){ 
+        stdout.write('-1'); 
+    } else if (answer === 0) {
+        stdout.write('0'); 
+    } else stdout.write('1'); 
     process.exit();
 });
